c14ecd474187d9a74b797067c4f935fabbb34aaf
Julian <develop@jooami.com>
2021-09-21 07:47:28 -0400

    svelte pokedex

---
+++ src/routes/about.svelte
@@ -0,0 +1,31 @@
+<svelte:head>
+	<title>About - Sveltkit Pokedex</title>
+</svelte:head>
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
+<p class="mb-4">
+	<strong
+		>This is a Sveltekit demo that uses the Pokemon API to build a Pokemon Rolodex, or, a Pokedex.</strong
+	>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum consectetur nesciunt itaque minima.
+	Dolorem voluptatibus illum in neque, deleniti quae ducimus necessitatibus aliquid incidunt provident
+	unde asperiores dignissimos, possimus inventore molestiae assumenda facilis. Consequuntur fuga quidem
+	impedit error incidunt iure non quos eum neque reiciendis quam, aut exercitationem quo laudantium.
+</p>
+<p class="mb-4">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque natus corrupti quisquam
+	provident at, est sapiente fugit itaque, optio odit nostrum quos tenetur sunt nesciunt alias
+	distinctio maiores pariatur minus sint similique vitae adipisci nam facere vel. Atque officia, in
+	adipisci id modi laudantium impedit nesciunt laborum molestias repellat doloremque est placeat
+	sapiente, mollitia nostrum rem excepturi reprehenderit ullam aspernatur quos dignissimos nemo!
+	Alias molestias, odit rerum culpa amet quaerat atque optio consequuntur dolor placeat illum,
+	deleniti accusamus earum molestiae et beatae dicta, explicabo fugiat soluta. Vero harum itaque ut
+	assumenda reprehenderit. Nobis dolores officia beatae, dicta laboriosam atque mollitia.
+</p>
+<p>
+	Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima veniam labore earum magnam
+	similique quos, dolorum quis nihil ab sunt pariatur consectetur nostrum, itaque ut velit. Sit
+	excepturi suscipit saepe cupiditate tempore temporibus nisi quasi eos mollitia quis maxime
+	eligendi eum vitae odio aspernatur at ad necessitatibus magni inventore sapiente perferendis
+	atque, dignissimos repellat. Repellendus facilis, culpa natus quis animi deserunt rerum modi.
+	Explicabo eum minima libero vel nemo, iusto ea earum ut quisquam iste?
+</p>
